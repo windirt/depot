d3a10f116562377667cddac65a069f30152a75b1
Windirt Young <windirt@msn.com>
2015-09-01 21:48:43 +0800

    finish mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,5 @@ Depot::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.action_mailer.delivery_method = :test
 end
